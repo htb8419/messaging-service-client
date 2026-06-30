6f9a477e940df1c92951b257f8f6df3be8818fdd
HT <tayebi1426@gmail.com>
2026-06-30 21:04:33 +0330

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 export { MessagingClient } from './MessagingClient'
 export { EventType } from './events/EventType'
 
+// Enum const objects (runtime values for JS consumers) + their types
+export { ConnectionState } from './enums/ConnectionState'
+export type { ConnectionState as ConnectionStateType } from './enums/ConnectionState'
+export { CallState } from './enums/CallState'
+export type { CallState as CallStateType } from './enums/CallState'
+export { PresenceState } from './enums/PresenceState'
+export type { PresenceState as PresenceStateType } from './enums/PresenceState'
+export { TypingState } from './enums/TypingState'
+export type { TypingState as TypingStateType } from './enums/TypingState'
+export { EventSubType } from './enums/EventSubType'
+export type { EventSubType as EventSubTypeType } from './enums/EventSubType'
+
 // Types for consumers
 export type { MessagingOptions, RtcConfig, RtcIceServer, MediaConstraints } from './core/Config'
 export type {
@@ -14,8 +26,3 @@ export type {
   MessagingError,
   MessagingEventMap,
 } from './events/types'
-export type { ConnectionState } from './enums/ConnectionState'
-export type { CallState } from './enums/CallState'
-export type { PresenceState } from './enums/PresenceState'
-export type { TypingState } from './enums/TypingState'
-export type { EventSubType } from './enums/EventSubType'
